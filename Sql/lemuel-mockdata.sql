92e85661b21240995e1782babf4f13ec87f28dfb
LemuelThomas <lemuel.thomas3@gmail.com>
2022-05-26 17:32:01 -0400

    Week 2 Project Fixed

--- Sql/lemuel-mockdata.sql
+++ Sql/lemuel-mockdata.sql
@@ -1,13 +1,17 @@
 set search_path to Netflix_app;
 
-insert into netflix_acc (FirstName, LastName, email, password, role_id) values ('Lemuel', 'Thomas', 'lemuel.thomas3@gmail.com', 'demopassword1', 3);
-insert into netflix_acc (FirstName, LastName, email, password, role_id) values ('Christopher', 'King', 'cking22@example.com', 'demopassword2', 2);
-insert into netflix_acc (FirstName, LastName, email, password, role_id) values ('Michael', 'Mons', 'MichMons@yahoo.com', 'demopassword3', 1);
+insert into subscription_roles (role_Name) values ('basic_user');
+insert into subscription_roles (role_Name) values ('hd_user');
+insert into subscription_roles (role_Name) values ('ultrahd_user');
+insert into user_roles (user_category) values ('adult');
+insert into user_roles (user_category) values ('teen');
+insert into user_roles (user_category) values ('child');
 
-insert into netflix_user (Name, Age, role_id) values ('YoungerBrother', 15, 2);
-insert into netflix_user (Name, Age, role_id) values ('OlderBrother', 30, 1);
-insert into netflix_user (Name, Age, role_id) values ('Mom', 40, 1);
-insert into netflix_user (Name, Age, role_id) values ('Dad', 43, 1);
+insert into netflix_acc (FirstName, LastName, email, username, password, acc_id) values ('Lemuel', 'Thomas', 'lemuel.thomas3@gmail.com', 'lemstry', 'demopassword1', 3);
+insert into netflix_acc (FirstName, LastName, email, username, password, acc_id) values ('Christopher', 'King', 'cking22@example.com', 'ckingster' , 'demopassword2', 2);
+insert into netflix_acc (FirstName, LastName, email, username, password, acc_id) values ('Michael', 'Mons', 'MichMons@yahoo.com', 'nmons12' ,'demopassword3', 1);
 
-insert into subscription_roles (role_Name) values ('basic_user', 'hd_user', 'ultrahd_user');
-insert into user_roles (role_Name) values ('adult', 'child', 'teen');
+insert into netflix_user (Name, Age, user_id) values ('YoungerBrother', 15, 2);
+insert into netflix_user (Name, Age, user_id) values ('OlderBrother', 30, 1);
+insert into netflix_user (Name, Age, user_id) values ('Mom', 40, 1);
+insert into netflix_user (Name, Age, user_id) values ('Dad', 43, 1);
--- Sql/lemuel-table-creation.sql
+++ Sql/lemuel-table-creation.sql
@@ -4,27 +4,33 @@ set search_path to Netflix_app;
 
 create table subscription_roles (
     id int generated always as identity primary key,
-    role_Name varchar unique not null,
+    role_Name varchar unique not null
 );
 
 create table user_roles (
     id int generated always as identity primary key,
-    category varchar not null
+    user_category varchar not null
 );
 
 create table netflix_user (
-    id int generated always as identity primary key,
+    netflix_id int generated always as identity primary key,
     name varchar unique not null,
     age int not null,
-    user_id int foreign key references user_roles(id)
-);
+    user_id int,
+    constraint fk_user
+    foreign key (user_id)
+    references user_roles(id)
+    );
 
 create table netflix_acc (
-    acc.id int generated always as  identity primary key,
+    id int generated always as  identity primary key,
     FirstName varchar not null,
     LastName varchar not null,
     email varchar unique not null,
-    username varchar unique not null check(length(username) >= 5)
-    password varchar not null check(length(password) >= 8)
-    sub_id int foreign key references subscription_roles(id)
+    username varchar unique not null check(length(username) >= 5),
+    password varchar not null check(length(password) >= 8),
+    acc_id int,
+    constraint fk_acc
+    foreign key (acc_id)
+    references subscription_roles(id)
 );
